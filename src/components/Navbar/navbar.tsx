3da1ab126ba2d9de03252fd7cd94cb74ceab05e9
Madaminbek <abduhoshimov1995@gmail.com>
2026-01-10 12:45:29 +0500

    Yangilandi navbar

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -2,6 +2,9 @@
 import Avatarka from "@/assets/icons/user.svg"
 import Logo from "@/assets/images/logoAgro.png"
 import { Link } from "react-router-dom"
+import TG from "@/assets/icons/tg.svg"
+import Insta from "@/assets/icons/insta.svg"
+import Face from "@/assets/icons/face.svg"
 import {
   Select,
   SelectContent,
@@ -12,29 +15,40 @@ import {
 } from "@/components/ui/select"
 const Navbar = () => {
   return (
-    <div className="mx-auto max-w-[1440px] h-[60px] mt-3 border px-5  rounded-lg">
-      <div className="flex items-center justify-between">
-        <img className="w-[80px]" src={Logo} alt="" />
-        <nav className="flex items-center justify-center gap-6">
-          <Link className="hover:text-[#00cc69] relative
+    <div className="mx-auto max-w-[1440px] h-[60px] mt-3 bg-[#FFFFFF19] shadow-sm border-black border px-5  rounded-lg">
+      <div className="flex items-center">
+        <img width="80" height="80" src={Logo} alt="" />
+        <div className="flex items-center gap-1 ml-30">
+          <div className="w-[40px] h-[40px] rounded-full bg-[#FFFFFF19] p-2">
+            <img width="35" height="35" src={TG} alt="" />
+          </div>
+          <div className="w-[40px] h-[40px] rounded-full bg-[#FFFFFF19] p-2">
+            <img width="35" height="35" src={Insta} alt="" />
+          </div>
+          <div className="w-[40px] h-[40px] rounded-full bg-[#FFFFFF19] p-2">
+            <img width="35" height="35" src={Face} alt="" />
+          </div>
+        </div>
+        <nav className="flex items-center justify-center gap-10 ml-50">
+          <Link className="hover:text-black relative
     inline-block
-    text-black
+    text-white
     after:content-['']
     after:absolute
     after:left-1/2
     after:-bottom-1
     after:h-[2px]
     after:w-0
-    after:bg-gradient-to-r
-    after:from-[#00cc69]
+    after:bg-gradient-to-l
+    after:from-white
     after:to-[#00cc69]
     after:transition-all
     after:duration-300
     hover:after:w-full
     hover:after:left-0" to="/">Home</Link>
-          <Link className="hover:text-[#00cc69] relative
+          <Link className="hover:text-black relative
     inline-block
-    text-black
+    text-white
     after:content-['']
     after:absolute
     after:left-1/2
@@ -42,15 +56,15 @@ const Navbar = () => {
     after:h-[2px]
     after:w-0
     after:bg-gradient-to-r
-    after:from-[#00cc69]
+    after:from-white
     after:to-[#00cc69]
     after:transition-all
     after:duration-300
     hover:after:w-full
     hover:after:left-0" to="/product">Product</Link>
-          <Link className="hover:text-[#00cc69] relative
+          <Link className="hover:text-black relative
     inline-block
-    text-black
+    text-white
     after:content-['']
     after:absolute
     after:left-1/2
@@ -58,15 +72,15 @@ const Navbar = () => {
     after:h-[2px]
     after:w-0
     after:bg-gradient-to-r
-    after:from-[#00cc69]
+    after:from-white
     after:to-[#00cc69]
     after:transition-all
     after:duration-300
     hover:after:w-full
     hover:after:left-0" to="/services">Services</Link>
-          <Link className="hover:text-[#00cc69] relative
+          <Link className="hover:text-black relative
     inline-block
-    text-black
+    text-white
     after:content-['']
     after:absolute
     after:left-1/2
@@ -74,17 +88,17 @@ const Navbar = () => {
     after:h-[2px]
     after:w-0
     after:bg-gradient-to-r
-    after:from-[#00cc69]
+    after:from-white
     after:to-[#00cc69]
     after:transition-all
     after:duration-300
     hover:after:w-full
     hover:after:left-0" to="/blog">About</Link>
         </nav>
-        <div className="flex items-center gap-6">
-          <div className="flex items-center gap-5">
+        <div className="flex items-center gap-6 ">
+          <div className="flex items-center gap-6 ml-100">
             <Select>
-              <SelectTrigger className="w-[70px]">
+              <SelectTrigger className="w-[70px] border-white text-white">
                 <SelectValue placeholder="UZ" />
               </SelectTrigger>
               <SelectContent>
@@ -95,7 +109,7 @@ const Navbar = () => {
               </SelectContent>
             </Select>
             <div className="flex items-center gap-3">
-              <div className="w-[40px] h-[40px] rounded-full bg-[#00cc69]">
+              <div className="w-[40px] h-[40px] rounded-full bg-[#FFFFFF19]">
                 <img className="rounded-full p-2" src={Avatarka} />
               </div>
             </div>
